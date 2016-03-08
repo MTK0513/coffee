ddcd1416ee1f848b42060090077d382b4f9b9184
MTK0513 <motoki.yajima@gmail.com>
2016-03-08 18:15:09 +0900

    ファイル結合の結果ファイルの追加と不要ファイル削除

--- src/assets/js/all.js
+++ src/assets/js/all.js
@@ -3,22 +3,6 @@
     extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
     hasProp = {}.hasOwnProperty;
 
-  Number.prototype.center = function() {
-    return this.half();
-  };
-
-  Number.prototype.half = function() {
-    return this / 2;
-  };
-
-  canvasMain = $('#mycanvas1')[0];
-
-  canvasBuff = $('#mycanvas2')[0];
-
-  ctxMain = canvasMain.getContext('2d');
-
-  ctxBuff = canvasBuff.getContext('2d');
-
   Actor = (function() {
     function Actor(x1, y1, width, height) {
       this.x = x1;
@@ -30,34 +14,6 @@
     return Actor;
 
   })();
-
-  Player = (function(superClass) {
-    extend(Player, superClass);
-
-    function Player() {
-      this.img = new Image();
-      this.img.src = "./assets/img/Blinky.png";
-      this.bullets = [];
-      Player.__super__.constructor.call(this, canvasMain.width.center() - 25, canvasMain.height - 50, 50, 50);
-    }
-
-    Player.prototype.update = function(e) {
-      var mouseX, mouseY, rect;
-      rect = e.target.getBoundingClientRect();
-      mouseX = e.clientX - rect.left;
-      mouseY = e.clientY - rect.top;
-      this.x = mouseX - 25;
-      return this.y = mouseY - 25;
-    };
-
-    Player.prototype.shoot = function() {
-      this.bullets.push(new Bullet(this.x, this.y, 20));
-      return console.log(this.bullets.length);
-    };
-
-    return Player;
-
-  })(Actor);
 
   Bullet = (function(superClass) {
     extend(Bullet, superClass);
@@ -76,10 +32,56 @@
 
   })(Actor);
 
+  Number.prototype.center = function() {
+    return this.half();
+  };
+
+  Number.prototype.half = function() {
+    return this / 2;
+  };
+
+  Player = (function(superClass) {
+    extend(Player, superClass);
+
+    function Player(x, y) {
+      this.img = new Image();
+      this.img.src = "./assets/img/Blinky.png";
+      this.mouseX = 0;
+      this.mouseY = 0;
+      this.bullets = [];
+      Player.__super__.constructor.call(this, x, y, 50, 50);
+    }
+
+    Player.prototype.update = function(e) {
+      var rect;
+      rect = e.target.getBoundingClientRect();
+      this.mouseX = e.clientX - rect.left;
+      this.mouseY = e.clientY - rect.top;
+      this.x = this.mouseX - 25;
+      return this.y = this.mouseY - 25;
+    };
+
+    Player.prototype.shoot = function() {
+      this.bullets.push(new Bullet(this.mouseX, this.mouseY, 20));
+      return console.log(this.bullets.length);
+    };
+
+    return Player;
+
+  })(Actor);
+
+  canvasMain = $('#mycanvas1')[0];
+
+  canvasBuff = $('#mycanvas2')[0];
+
+  ctxMain = canvasMain.getContext('2d');
+
+  ctxBuff = canvasBuff.getContext('2d');
+
   $('#start-btn').click(function() {
-    var main, player;
+    var drawBullet, main, player;
     ctxMain.clearRect(0, 0, canvasMain.width, canvasMain.height);
-    player = new Player();
+    player = new Player(canvasMain.width.center() - 25, canvasMain.height - 50);
     player.img.onload = function() {
       return ctxMain.drawImage(player.img, player.x, player.y, player.width, player.height);
     };
@@ -92,16 +94,19 @@
       results = [];
       for (i = 0, len = ref.length; i < len; i++) {
         bullet = ref[i];
-        ctxMain.beginPath();
-        ctxMain.fillStyle = bullet.style;
-        ctxMain.arc(bullet.x, bullet.y, 20, 0, Math.PI * 2, false);
-        results.push(ctxMain.fill());
+        results.push(drawBullet(bullet));
       }
       return results;
     }, false);
     canvasMain.addEventListener('click', function(e) {
       return player.shoot();
     }, false);
+    drawBullet = function(bullet) {
+      ctxMain.beginPath();
+      ctxMain.fillStyle = bullet.style;
+      ctxMain.arc(bullet.x, bullet.y, 20, 0, Math.PI * 2, false);
+      return ctxMain.fill();
+    };
     main = function() {
       var bullet, i, len, ref;
       ctxMain.clearRect(0, 0, canvasMain.width, canvasMain.height);
@@ -110,10 +115,7 @@
       for (i = 0, len = ref.length; i < len; i++) {
         bullet = ref[i];
         bullet.move();
-        ctxMain.beginPath();
-        ctxMain.fillStyle = bullet.style;
-        ctxMain.arc(bullet.x, bullet.y, 20, 0, Math.PI * 2, false);
-        ctxMain.fill();
+        drawBullet(bullet);
       }
       return setTimeout(main, 20);
     };
